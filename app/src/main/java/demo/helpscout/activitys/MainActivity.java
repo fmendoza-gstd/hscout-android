d4937bee6e4df778b333fe1b160f16bf2b73fa14
gvazquez-gstd <gvazquez@globalstd.com>
2016-12-16 15:03:00 -0600

    Menu Support

--- app/src/main/java/demo/helpscout/activitys/MainActivity.java
+++ app/src/main/java/demo/helpscout/activitys/MainActivity.java
@@ -1,13 +1,20 @@
 package demo.helpscout.activitys;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 import demo.helpscout.R;
 import fragments.ConversationsFragment2;
@@ -25,9 +32,54 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //setToolbar(); // Setear Toolbar como action bar
+        setToolbar(); // Setear Toolbar como action bar
 
-        selectItem("conversations");
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        //navigationView.setNavigationItemSelectedListener(this);
+        if (navigationView != null) {
+            setupDrawerContent(navigationView);
+        }
+
+       // drawerTitle = getResources().getString(R.string.welcome);
+        if (savedInstanceState == null) {
+            selectItem("Bienvenido");
+            loadNavHeader();
+        }
+    }
+
+    /***
+     * Load navigation menu header information
+     * like background image, profile image
+     * name, website, notifications action view (dot)
+     */
+    private void loadNavHeader() {
+        /*
+        // name, website
+        txtName.setText("Ravi Tamada");
+        txtWebsite.setText("www.androidhive.info");
+
+        // loading header background image
+        Glide.with(this).load(urlNavHeaderBg)
+                .crossFade()
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .into(imgNavHeaderBg);
+
+        // Loading profile image
+        Glide.with(this).load(urlProfileImg)
+                .crossFade()
+                .thumbnail(0.5f)
+                .bitmapTransform(new CircleTransform(this))
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .into(imgProfile);
+
+        */
+
+        // showing dot next to notifications label
+        //navigationView.getMenu().getItem(3).setActionView(R.layout.menu_dot);
+        navigationView.getMenu().getItem(3).setActionView(R.layout.menu_counter);
+        //TextView view = (TextView) navigationView.getMenu().findItem(1).getActionView();
+        //view.setText(6 > 0 ? String.valueOf(6) : null);
     }
 
     private void setToolbar() {
@@ -36,9 +88,55 @@ public class MainActivity extends AppCompatActivity {
         final ActionBar ab = getSupportActionBar();
         if (ab != null) {
             // Poner ícono del drawer toggle
-            //ab.setHomeAsUpIndicator(R.mipmap.ic_menu);
+            ab.setHomeAsUpIndicator(R.mipmap.ic_menu);
             ab.setDisplayHomeAsUpEnabled(true);
         }
+    }
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        // Marcar item presionado
+                        menuItem.setChecked(true);
+                        // Crear nuevo fragmento
+                        String title = menuItem.getTitle().toString();
+                        selectItem(title);
+                        mTitle = title;
+                        return true;
+                    }
+                }
+        );
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        if (!drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            getMenuInflater().inflate(R.menu.activity_main_drawer, menu);
+            restoreActionBar();
+            return true;
+        }
+        return super.onCreateOptionsMenu(menu);
+    }
+    public void restoreActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_STANDARD);
+        actionBar.setDisplayShowTitleEnabled(true);
+        actionBar.setTitle(mTitle);
+        //actionBar.setLogo(R.drawable.icon_header);
+        actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor(ACTIONBAR_COLOR)));//Cambia el color de fondo de ActionBar
+    }
+
+    /* btn Back in fragments */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private void selectItem(String opcSelected) {
---
+++ app/src/main/java/fragments/MailBoxFragment.java
@@ -0,0 +1,7 @@
+package fragments;
+
+/**
+ * Created by Gabriel on 16/12/2016.
+ */
+public class MailBoxFragment {
+}
